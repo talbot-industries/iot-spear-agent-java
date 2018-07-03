0506379f8521f7dfaac7c817af44d6e080c7150e
Talbot Industries Ltd <info@talbot-industries.com>
2018-07-03 22:04:09 +0100

    Added startup banner

--- src/main/java/com/iotspear/agent/EntryPoint.java
+++ src/main/java/com/iotspear/agent/EntryPoint.java
@@ -2,11 +2,16 @@ package com.iotspear.agent;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import com.google.common.base.Charsets;
+import com.google.common.io.CharStreams;
 import com.google.inject.Guice;
 import com.iotspear.agent.data.LoggingConfig;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @Slf4j
 public class EntryPoint {
@@ -14,6 +19,7 @@ public class EntryPoint {
     public static void main(String[] args) {
 
         log.info("Started IoT Spear Agent ...");
+        log.info(getBannerText());
 
         run(new Configuration());
     }
@@ -30,5 +36,17 @@ public class EntryPoint {
         }
 
         injector.getInstance(Processor.class).initiate();
+    }
+
+    private static String getBannerText() {
+
+        try (val resource = EntryPoint.class.getResourceAsStream("/banner.txt")) { // try-with-resources
+
+            return CharStreams.toString(new InputStreamReader(resource, Charsets.UTF_8));
+        }
+        catch (IOException ex) {
+
+            return "";
+        }
     }
 }
